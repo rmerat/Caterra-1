3d5a6f9c5b32252cc6492ab1ca74cfe56b1ec3b6
Roxane Mérat <rmerat@student.ethz.ch>
2022-12-30 22:30:00 +0100

    start .py code

--- CompletePyCode/MaskingProcess.py
+++ CompletePyCode/MaskingProcess.py
@@ -1,8 +1,22 @@
-def obtain_name_images(image_folder):
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from PIL import Image 
+import os
+import ImageAnnotation
 
+VID = 0
+SING_IMG = 1
+nb_row = 4
+
+# READ AND NORMALIZE IMAGES
+def obtain_name_images(image_folder):
+    """
+    input : name of the folder with the images
+    output : name of the files to be open
+    """
     lst = os.listdir(image_folder)
     lst.sort()
-
     name_images = [img for img in lst 
                 if img.endswith(".jpg") or
                     img.endswith(".jpeg") or
@@ -11,13 +25,17 @@ def obtain_name_images(image_folder):
     return name_images 
 
 def obtain_images(name_images, image_folder, mode):
+    """
+    input : name and foler of the images location
+    returns : list containing all the images in the folder
+    """
     imgs = []
-
     if mode == VID : 
         for name in name_images:
             img = cv2.imread(os.path.join(image_folder, name))
             if img is not None : 
-                img_small = img_resize(img)
+                img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                img_small = img_resize(img_rgb)
                 imgs.append(img_small)
 
     if mode == SING_IMG :
@@ -35,12 +53,14 @@ def obtain_images(name_images, image_folder, mode):
     return None
 
 
-def img_resize(img):
-    #resize
-    output_width = 900  #set the output size
+def img_resize(img, output_width = 900):
+    """
+    input : image
+    output : resized image
+    """
     wpercent = (output_width/float(img.shape[1]))
     hsize = int((float(img.shape[0])*float(wpercent)))
-    img = cv2.resize(img, (output_width,hsize), interpolation = cv2.INTER_AREA) #this resize makes the video not work??
+    img = cv2.resize(img, (output_width,hsize), interpolation = cv2.INTER_AREA) 
+    #this resize makes the video not work??
     #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
     return img
