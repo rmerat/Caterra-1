fixed evaluation formula but saving data is taking too many time same points

CompletePyCode/MaskingProcess.py
@@ -58,7 +58,7 @@ def obtain_images(name_images, image_folder, mode):
         img = cv2.imread(os.path.join(image_folder, name_images))
         if img is not None : 
             img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img_small = img_resize(img_rgb, output_width=900)
+            img_small = img_resize(img_rgb, output_width=320)
             imgs.append(img_small)
         else : 
             print('no image read')
@@ -224,7 +224,7 @@ def hough_line_improved(mask, angle_acc):
             # Calculate rho. diag_len is added for a positive index
             rho = round(x * cos_t[t_idx] + y * sin_t[t_idx]) + diag_len
             accumulator[rho, t_idx] += 1
-            if (abs(np.rad2deg(thetas[t_idx])-90)<20): #if horizontale lignes 
+            if (abs(np.rad2deg(thetas[t_idx])-90)<25): #if horizontale lignes 
                 accumulator[:, t_idx] = 0
             
             for angle in angle_acc:
@@ -337,7 +337,7 @@ def apply_ransac(img_no_sky, masked_images_i, vp_point, vp_on, best_mask, arr_ma
 
 
     if(data.shape[0]>700):
-        data = data[np.random.choice(data.shape[0], 600, replace=False), :]
+        data = data[np.random.choice(data.shape[0], 500, replace=False), :]
 
     #put condition, if data to small, go to initial process!
     if(data.shape[0]<50):
@@ -399,7 +399,7 @@ def remove_double(p1, p2, m, acc_m, masked_image, wd):
     return masked_image, cond_double
 
 def remove_horizon(p1, p2, m, masked_image, bw):
-    thr = 0.1
+    thr = 0.2 #before : 0.1
     cond_horizon = 0
 
     if (abs(m)<thr):
@@ -480,8 +480,6 @@ def pattern_ransac(arr_mask, vp_point, img, max_iterations=100, threshold=2000):
     print('nb inliers : ', nb_inliers, 'out of ', nb_cr * len(x))
     
     return model
-
-
 
 def intersect_multiple_lines(pts1,pts2):
     """P0 and P1 are NxD arrays defining N lines.

CompletePyCode/PerImageProcessing.py
@@ -74,7 +74,7 @@ def check_equidistance(arr_mask):
 
     return arr_mask
 
-def speed_process_lines(image, col_best_mask, arr_mask, vp_pt):
+def speed_process_lines(image, col_best_mask, arr_mask, vp_pt, vp_on):
 
     img_lab = skimage.color.rgb2lab(image/255) #calculate best color mask based on previously calculated color 
     col_best_mask_lab = skimage.color.rgb2lab((col_best_mask[0]/255, col_best_mask[1]/255, col_best_mask[2]/255))
@@ -83,8 +83,6 @@ def speed_process_lines(image, col_best_mask, arr_mask, vp_pt):
     #plt.imshow(mask_col)
     band_width = int(image.shape[1]/25)
     #print('bw : ', band_width)
-    
-
     # mask_col_edge = cv2.Canny(mask_col,100,200) #add la cond sur le laplacien 
 
     pts1 = []
@@ -117,13 +115,13 @@ def speed_process_lines(image, col_best_mask, arr_mask, vp_pt):
 
         
         while(cond_horizon*cond_double == 0 ): 
-            p1, p2, m, cond_speed = MaskingProcess.apply_ransac(image, masked_images[i], vp_pt, 0, best_mask, arr_mask[i], i)
+            p1, p2, m, cond_speed = MaskingProcess.apply_ransac(image, masked_images[i], vp_pt, vp_on, best_mask, arr_mask[i], i) #HERE JUST CHANGED 
             
             if (cond_speed==0): 
                 print('reinitialization')
                 #reinitialize(img_lab, col_best_mask_lab)
                 #p1, p2, m, cond_speed = MaskingProcess.apply_ransac(image, masked_images[i], vp_pt, 0, best_mask, arr_mask[i], i)
-                return arr_mask_new, img_ransac_lines, vp_pt, cond_speed
+                return arr_mask_new, img_ransac_lines, vp_pt, cond_speed, crops_only
 
             
             masked_images[i], cond_horizon = MaskingProcess.remove_horizon(p1, p2, m, masked_images[i], band_width)
@@ -146,47 +144,5 @@ def speed_process_lines(image, col_best_mask, arr_mask, vp_pt):
 
     #cv2.imshow('ransac lines : ', img_ransac_lines)
 
-    return arr_mask_new, img_ransac_lines, vp_pt, 1, crops_only 
+    return arr_mask_new, img_ransac_lines, vp_pt, 1, crops_only, pts1, pts2
 
-"""
-def reinitialize(img_lab, col_best_mask_lab): 
-    
-    print('initial process...')
-
-    hough_img, arr_mask, col_best_mask, vp_pt = Initial_Process(images[0], nb_row = nb_row, sky = sky)
-
-
-    best_mask_median, col_best_mask = MaskingProcess.veg_segmentation(img, img_no_sky)
-    best_mask_median_edge = cv2.Canny(best_mask_median,100,200)
-
-    arr_mask, th_acc, r_acc, threshold_acc, best_mask_evaluate = MaskingProcess.keep_mask_max_acc_lines(best_mask_median_edge, img_no_sky, nb_row)
-
-    vp_pt = np.asarray(MaskingProcess.VP_detection(th_acc, r_acc, threshold_acc, best_mask_median_edge))
-
-    #cv2.imshow('after initial process', best_mask_evaluate)
-    #cv2.waitKey(0)
-
-    arr_mask = check_equidistance(arr_mask)
-
-
-
-
-    height_original = images[0].shape[0]
-    height, _, = arr_mask[0].shape
-
-    return image, col_best_mask, arr_mask, vp_pt
-
-    return 0
-
-
-
-        print('initial process...')
-        hough_img, arr_mask, col_best_mask, vp_pt = PerImageProcessing.Initial_Process(images[0], nb_row = nb_row, sky = sky)
-        height_original = images[0].shape[0]
-        height, _, = arr_mask[0].shape
-        
-        #cv2.imshow('hough image :  :q ', cv2.cvtColor(hough_img, cv2.COLOR_RGB2BGR))
-        #cv2.waitKey(0)
-        #if cv2.waitKey(1) == ord('q'):
-            #cv2.destroyAllWindows() 
-"""